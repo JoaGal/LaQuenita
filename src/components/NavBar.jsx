102d90d3d2173401cc73b356402eedb7b46afc26
JoaGal <joaquingaldeano7@gmail.com>
2025-05-01 21:56:02 -0300

    Improve navbar icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -51,28 +50,27 @@ export const NavBar = () => {
 
         <ul className="icons">
           <li>
-            <Link href="#" className="icon brands fa-facebook-f">
+            <a href="#" className="icon brands fa-facebook-f">
               <span className="label">Facebook</span>
-            </Link>
+            </a>
           </li>
           <li>
-            <Link
-              href="https://www.instagram.com/laquenita_"
+            <a href="https://www.instagram.com/laquenita_"
               className="icon brands fa-instagram"
               target="_blank"
             >
               <span className="label">Instagram</span>
-            </Link>
+            </a>
           </li>
           <li>
-            <Link href="#" className="icon solid fa-envelope">
-              <span className="label">Email</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="#" className="icon brands fa-github">
+            <a href="https://wa.me/543624712344" className="icon brands fa-whatsapp">
               <span className="label">GitHub</span>
-            </Link>
+            </a>
+          </li>
+          <li>
+            <a href="#message" className="icon solid fa-envelope">
+              <span className="label">Email</span>
+            </a>
           </li>
         </ul>
       </nav>
